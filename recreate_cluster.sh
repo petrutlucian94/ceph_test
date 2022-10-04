53c43188e39f46fe30222986f68021a6d7f78602
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2022-10-04 14:05:05 +0300

    update recreate_cluster.sh script

--- recreate_cluster.sh
+++ recreate_cluster.sh
@@ -1,18 +1,20 @@
 set -ex
 
-CEPH_BUILD_DIR=${CEPH_BUILD_DIR:-"/home/ubuntu/workspace_local/ceph_active_build"}
+CEPH_BUILD_DIR=${CEPH_BUILD_DIR:-`realpath /mnt/sdb1/ceph.linux/build`}
 
 cd "$CEPH_BUILD_DIR"
+export PATH=$PATH:$CEPH_BUILD_DIR
 
 ../src/stop.sh
 
 DEBUG=${DEBUG:-}
 CLEAN=${CLEAN:-}
 SKIP_DEFAULT_IMAGES=${SKIP_DEFAULT_IMAGES:-}
-IP=${IP:-11.11.0.251}
-VSTART_DIR=${VSTART_DIR:-"/mnt/ramdisk/vstart.memstore"}
+IP=${IP:-192.168.122.1}
+VSTART_DIR=${VSTART_DIR:-"/mnt/sdb1/vstart"}
 MEMSTORE_BYTES=${MEMSTORE_BYTES:-16106127360}
 BIND_IPV6=${BIND_IPV6:-"false"}
+USE_MEMSTORE=${USE_MEMSTORE:-}
 
 if [[ -n $DEBUG ]]; then
     dbg_flag="-d"
@@ -27,8 +29,14 @@ fi
 mkdir -p $VSTART_DIR/out
 
 ../src/stop.sh
+
+memstore_args=""
+if [[ -n $USE_MEMSTORE ]]; then
+    memstore_args="--memstore -o \"memstore_device_bytes=$MEMSTORE_BYTES\""
+fi
+
 OSD_POOL_DEFAULT_SIZE=1 VSTART_DEST=$VSTART_DIR ../src/vstart.sh \
-    $dbg_flag $clean_flag --memstore -o "memstore_device_bytes=$MEMSTORE_BYTES" \
+    $dbg_flag $clean_flag $memstore_args \
     -o "ms bind ipv6 = $BIND_IPV6" \
     -i $IP \
     2>&1 | tee $VSTART_DIR/vstart.log
@@ -36,6 +44,7 @@ OSD_POOL_DEFAULT_SIZE=1 VSTART_DEST=$VSTART_DIR ../src/vstart.sh \
 export CEPH_CONF=$VSTART_DIR/ceph.conf
 export CEPH_KEYRING=$VSTART_DIR/keyring
 
+sudo mkdir -p /etc/ceph
 sudo cp $CEPH_CONF /etc/ceph/
 sudo cp $CEPH_KEYRING /etc/ceph
 # It's just a test env, let's make this easy to transfer.
